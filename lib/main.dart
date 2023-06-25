d28a36a0cbf0247a70a157bc381d96bde438c522
macbook <macbook@macbooks-MacBook-Pro.local>
2023-06-25 12:40:57 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (context, child) {
+        return Scaffold(
+          body: Container(),
+        );
+      },
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
