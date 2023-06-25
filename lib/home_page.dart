ccf9d7055daa71483ab60fd335486521d0c041e3
macbook <macbook@macbooks-MacBook-Pro.local>
2023-06-25 19:33:20 +0800

    30

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:first_app/cart.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,28 +11,91 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String title = "Off";
+  final List<String> _cardList = ['A', "10", "J", "K"];
+  Timer? _timerController;
+  List<String> _showedCardList = [];
+  int _timer = 1000;
 
-  void onSubmit(String value) {
+  @override
+  void initState() {
+    super.initState();
+    _cardList.shuffle();
+    _timerController = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      if (_timer > 0)
+        setState(() {
+          _timer = _timer - 1;
+        });
+    });
+  }
+
+  void onClickCard(String title) {
+    if (title == "A") {
+      _timerController?.cancel();
+    }
     setState(() {
-      title = value;
+      _showedCardList.add(title);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Center(
-            child: Row(
-      mainAxisSize: MainAxisSize.max,
-      // crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text("First text"),
-        Text("Second text"),
-        Text("Third text"),
-      ],
-    )));
+        body: Stack(children: [
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: height * _timer / 1000,
+          color: Colors.amberAccent,
+        ),
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "Хугацаа: $_timer",
+            style: TextStyle(fontSize: 24),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyCard(
+                _cardList[0],
+                onTap: onClickCard,
+                active: _showedCardList.contains(_cardList[0]),
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              MyCard(
+                _cardList[1],
+                onTap: onClickCard,
+                active: _showedCardList.contains(_cardList[1]),
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyCard(
+                _cardList[2],
+                onTap: onClickCard,
+                active: _showedCardList.contains(_cardList[2]),
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              MyCard(
+                _cardList[3],
+                onTap: onClickCard,
+                active: _showedCardList.contains(_cardList[3]),
+              )
+            ],
+          )
+        ],
+      )
+    ]));
   }
 }
 
